a78aae68ec57346f8b254bb2bea8ca40d2daa85a
RIGOULET Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2019-12-17 06:39:13 +0100

    test

--- components/Image/image-item/index.js
+++ components/Image/image-item/index.js
@@ -1,19 +1,24 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
- class ImageItem extends React.Component {
+ const ImageItem = () => {
 
-
-	render() {
+	const [toggle, setToggle] = useState(false);
+	function onMouseOver(event){
+		console.log('d')
+		setToggle(!toggle);
+	}
 		return (
 			
 				<li className="thumbnail-image"  key={this.props.image._id}>
 					<img src={this.props.image.url}  
 					alt={this.props.image.name} 
 					style={{ height: '100%', width: '100%'}}
+					onMouseOver={()=>onMouseOver}
 				/>
+				,j
 				</li>
 			
 		);
 	}
-}
+
 export default ImageItem;
