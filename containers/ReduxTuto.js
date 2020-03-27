a0999573171c059801f235c280095243241e71cf
RIGOULET Henri-Pierre <rigoulet.henri.pierre@gmail.com>
2020-03-27 11:55:03 +0100

    remove

--- containers/ReduxTuto.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReduxComponent from '../components/Redux/ReduxComponent'
-const ReduxTuto = () => {
-  return (
-    <ReduxComponent />
-  )
-}
-
-export default ReduxTuto;
